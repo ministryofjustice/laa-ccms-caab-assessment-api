f3dcc894074cf336c2fc9746348e654777f9e472
Phil Payne <phil.payne@digital.justice.gov.uk>
2024-03-21 09:42:02 +0000

    Initial application structure

---
+++ assessment-service/src/main/java/uk/gov/laa/ccms/caab/assessment/AssessmentApplication.java
@@ -0,0 +1,15 @@
+package uk.gov.laa.ccms.caab.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point for the CAAB Assessment application.
+ */
+@SpringBootApplication
+public class AssessmentApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AssessmentApplication.class, args);
+  }
+
+}
